a101a947fae56c29a617ec9608d85aa63132a3e4
Masatoshi Nomura <nomum@yahoo.co.jp>
2017-05-13 12:11:40 +0900

    Easample for ps

---
+++ PS/Syntax/Function/function001.psl.ps1
@@ -0,0 +1,19 @@
+﻿Function AddArg($arg1 , $arg2){
+    $ret = $arg1 + $arg2
+    $arg1.getType()
+    $arg2.getType()
+    $ret.getType()
+    return $ret
+}
+
+Write-Output "test1"
+addArg 1 2
+
+#Write-Output "test2"
+#AddArg(3,"3");
+
+#Write-Output "test3"
+#AddArg("HELLO", 1111);
+
+#Write-Output "test4"
+#AddArg(1111,"Hello");
