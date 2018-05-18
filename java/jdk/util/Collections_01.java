35083c2e670a665d6764d4b44fc894ea72a4a637
nomura <nomura@netssc.co.jp>
2018-05-18 13:56:30 +0900

    aaa

---
+++ java/jdk/util/Collections_01.java
@@ -0,0 +1,16 @@
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
+
+public class Collections_01{
+
+    public static void main(String[] args){
+
+        Map<Integer , String> map =
+            Map.ofEntries(
+                Map.entry(1,"AAA")  
+            );
+
+    }
+
+}
